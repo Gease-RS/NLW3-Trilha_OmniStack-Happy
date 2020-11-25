65db28d493e49df8788c89da0de3c9697ab9566d
Gease-RS <geasebrasil@gmail.com>
2020-11-25 14:01:09 -0300

    Image View

--- backend/src/controllers/OrphanagesControllers.ts
+++ backend/src/controllers/OrphanagesControllers.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
+
+import orphanageView from '../views/orphanages_view'
 import Orphanage from '../models/Orphanage'
 
 
@@ -7,9 +9,11 @@ export default {
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage)
 
-        const orphanages = await orphanagesRepository.find()
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        })
 
-        return response.json(orphanages)
+        return response.json(orphanageView.renderMany(orphanages))
     },
 
     async show(request: Request, response: Response) {
@@ -17,9 +21,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage)
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id)
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        })
 
-        return response.json(orphanage)
+        return response.json(orphanageView.render(orphanage))
     },
 
     async create(request: Request, response: Response) {
@@ -30,7 +36,7 @@ export default {
             about,
             instructions,
             opening_hours,
-						open_on_weekends
+			open_on_weekends
         } = request.body
     
         const orphanagesRepository = getRepository(Orphanage)
@@ -48,8 +54,8 @@ export default {
             about,
             instructions,
             opening_hours,
-						open_on_weekends,
-						images
+			open_on_weekends,
+			images
         })
     
         await orphanagesRepository.save(orphanage)
