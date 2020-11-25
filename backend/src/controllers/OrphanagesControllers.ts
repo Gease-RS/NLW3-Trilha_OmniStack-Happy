d7e4bd58d91954965cdaef65cbdf60277501bbcd
Gease-RS <geasebrasil@gmail.com>
2020-11-24 23:30:35 -0300

    Upload Images

--- backend/src/controllers/OrphanagesControllers.ts
+++ backend/src/controllers/OrphanagesControllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanage'
 
+
 export default {
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage)
@@ -29,11 +30,17 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+						open_on_weekends
         } = request.body
     
         const orphanagesRepository = getRepository(Orphanage)
-    
+				
+				const requestImages = request.files as Express.Multer.File[]
+
+				const images = requestImages.map((image) => {
+					return { path: image.filename };
+				});
+				
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -41,7 +48,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+						open_on_weekends,
+						images
         })
     
         await orphanagesRepository.save(orphanage)
